41af9a7e7f7dc3403b00b0597cf79b07ac6b1f70
Ben Spencer <dangerous.ben@gmail.com>
2004-11-04 02:10:17 +0000

    Added some extra stats code for stuff I'm about to do.
    git-svn-id: http://lartem.googlecode.com/svn@42 a58511a1-b652-0410-89c8-73ab7e392364

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC = gcc -Wall -g
 LD = gcc -lpanel -lncurses -lefence -g
 
-OBJS = main.o display.o player.o role.o monst.o ask.o level.o map.o util.o
+OBJS = main.o display.o player.o role.o monst.o ask.o level.o map.o util.o stats.o
 
 lartem: $(OBJS)
 	$(LD) $(OBJS) -o lartem
--- stats.h
+++ stats.h
@@ -1,9 +1,22 @@
 #ifndef STATS_H
 #define STATS_H
 
+/* This should be in the correct order, in case we get bored and
+   decide to treat stats structs as arrays of unsigned ints */
+enum {
+	STAT_STR,
+	STAT_DEX,
+	STAT_CON,
+	STAT_INT,
+	STAT_WIS,
+	STAT_CHA
+};
+
 struct stats {
 	unsigned int st, dx, co, in, wi, ch;
 	unsigned int hp, hpmax;
 };
+
+int test_stat(struct stats *, unsigned int);
 
 #endif
